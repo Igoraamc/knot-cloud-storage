2795f533d99dc883a4728999c49e6a77d88baf4a
Igor <igor@guaiuba-006888.local>
2020-03-17 16:00:08 -0300

    main: Adds the main file
    This commit adds the file that get the configuration set, pass to the server, and calls the function to initialize the server.

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	. "github.com/Igoraamc/knot-cloud-storage/pkg/config"
+	"github.com/Igoraamc/knot-cloud-storage/pkg/controllers"
+	. "github.com/Igoraamc/knot-cloud-storage/pkg/interactor"
+	"github.com/Igoraamc/knot-cloud-storage/pkg/server"
+)
+
+var dao = DataDAO{}
+var config = Config{}
+
+func init() {
+	config.Read()
+
+	dao.Server = config.Server
+	dao.Database = config.Database
+	fmt.Println(dao.Server)
+	dao.Connect()
+}
+
+func main() {
+	dataController := controllers.NewDataController()
+
+	http := server.NewServer(config.Port, dataController)
+	http.Start()
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/gorilla/mux v1.7.4
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/urfave/negroni v1.0.0 // indirect
+	github.com/urfave/negroni v1.0.0
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 	gopkg.in/yaml.v2 v2.2.8 // indirect
