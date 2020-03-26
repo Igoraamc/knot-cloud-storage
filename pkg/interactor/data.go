5d2940c0c10c6e1140cb9fa8466a2d2dbd2484b4
Igor <igor@guaiuba-006888.local>
2020-03-26 17:07:14 -0300

    interactor: data: Refactor interactor
    - Interactor now receives the DataStore in his struct

--- pkg/interactor/data.go
+++ pkg/interactor/data.go
@@ -8,12 +8,12 @@ import (
 	. "github.com/CESARBR/knot-cloud-storage/pkg/entities"
 )
 
-var dataStore = data.NewDataStore()
+type DataInteractor struct {
+	DataStore data.DataStore
+}
 
-type DataInteractor struct{}
-
-func NewDataInteractor() *DataInteractor {
-	return &DataInteractor{}
+func NewDataInteractor(dataStore data.DataStore) DataInteractor {
+	return DataInteractor{dataStore}
 }
 
 func (d *DataInteractor) GetAll(order int, skip int, take int, startDate time.Time, finishDate time.Time) ([]Data, error) {
@@ -23,7 +23,7 @@ func (d *DataInteractor) GetAll(order int, skip int, take int, startDate time.Ti
 		selectOrder = "-timestamp"
 	}
 
-	data, err := dataStore.Get(selectOrder, skip, take, startDate, finishDate)
+	data, err := d.DataStore.Get(selectOrder, skip, take, startDate, finishDate)
 
 	return data, err
 }
@@ -37,14 +37,14 @@ func (d *DataInteractor) GetByID(id string, order int, skip int, take int, start
 
 	s, _ := strconv.ParseInt(id, 10, 64)
 
-	data, err := dataStore.Get(selectOrder, skip, take, startDate, finishDate)
+	data, err := d.DataStore.Get(selectOrder, skip, take, startDate, finishDate)
 
 	data = FilterDataBySensorID(data, int(s))
 	return data, err
 }
 
 func (d *DataInteractor) Save(data Data) error {
-	err := dataStore.Save(data)
+	err := d.DataStore.Save(data)
 	return err
 }
 
